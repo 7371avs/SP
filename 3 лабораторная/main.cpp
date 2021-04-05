8b2675351d3a4a98a5314990cf4cadb3c1ea27ea
7371avs <50544731+7371avs@users.noreply.github.com>
2021-04-05 15:41:47 +0700

    Add files via upload

---
+++ 3 лабораторная/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstdint>
+#include <cstddef>
+
+extern "C"{
+int foo(int *arr1, int *arr2, size_t size);
+}
+
+
+int main(void) {
+
+    int arr1[] = {1, 2, 3};
+    int arr2[] = {1, 2, 3};
+
+    size_t size = sizeof(arr1) / sizeof(int);
+
+    printf("%d\n", foo(arr1, arr2, size));
+
+    return 0;
+}
